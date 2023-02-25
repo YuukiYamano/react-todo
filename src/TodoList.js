9bb3bb897d7aa20477ea483633233b19d92eced0
YuukiYamano <yuukiyamano1108@gmail.com>
2023-02-25 14:13:03 +0900

    fix: change key

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Todo from './todo';
 
 const TodoList = ({todos}) => {
-  return todos.map((todo)=><Todo todo={todo} key={todo.name}/>)
+  return todos.map((todo)=><Todo todo={todo} key={todo.id}/>)
 };
 
 export default TodoList
