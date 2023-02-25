f9cafae630317e8c983bdb1cebf04aa71af00371
YuukiYamano <yuukiyamano1108@gmail.com>
2023-02-25 14:26:37 +0900

    feat: count uncomplete task func

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import TodoList from "./TodoList";
 import {v4 as uuidv4} from "uuid";
 
 function App() {
-  const [todos,setTodos] =useState([
-    {id:1,name: "Todo1", completed: false},
-  ]); 
+  const [todos,setTodos] =useState([]); 
 
 
   const todoNameRef = useRef();
@@ -32,7 +30,7 @@ function App() {
       <input type="text" ref={todoNameRef}></input>
       <button onClick={handleAddTodo}>タスクを追加</button>
       <button>完了したタスクの削除</button>
-      <div>残りのタスク:0
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}
       </div>
     </div>
   );
